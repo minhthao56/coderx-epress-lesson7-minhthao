116594de8dec64c9b9573da9b85d14cff40c2e73
Glitch (coderx-epress-lesson7-minhthaoal-soil) <none>
2020-05-09 10:07:07 +0000

    🐚📷 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,75 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const multer = require('multer') // v1.0.5
+const upload = multer() // for parsing multipart/form-data
+var low = require('lowdb')
+var FileSync = require('lowdb/adapters/FileSync')
+var shortid = require('shortid');
+
+var adapter = new FileSync('db.json')
+var db = low(adapter)
+db.defaults({ todos:[]}).write()
+
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+// https://expressjs.com/en/starter/basic-routing.html
+app.get('/', (request, response) => {
+  response.render('index',{name:'Minh Thao'});
+});
+
+
+app.get('/todos', (request, response) => {
+  response.render('todos/index',{
+    todos: db.get("todos").value()
+  });
+});
+
+
+app.get ('/todos/search', function(request, response){
+  var q = request.query.q;
+  var filterlisttodo = db.get("todos").filter(function(todo){
+    var lowercase = todo.text.toLowerCase();
+     return lowercase.indexOf(q) !==-1;
+  }).write();
+  console.log(filterlisttodo );
+  response.render('todos/index',{
+    todos: filterlisttodo
+  });
+});
+
+app.get('/todos/create', function(request, response){
+  response.render('create/index')
+});
+
+app.get('/todos/:todoid', function (request, response) {
+  
+  var id = parseInt(request.params.todoid);
+  var todo = db.get("todos").find({id: id}).value()
+  response.render("todos/viewid", {
+    todo: todo
+  })
+  
+});
+
+app.post('/todos/create', function(request, response){
+  db.get('todos').push(request.body).write()
+  request.body.id=shortid.generate();
+  response.redirect('/todos')
+});
+
+
+
+
+// listen for requests :)
+app.listen(process.env.PORT, () => {
+  console.log("Server listening on port " + process.env.PORT);
+});
